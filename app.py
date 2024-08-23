f03d60fe0aba87b88ee3af8ace4400f962d9a84a
Saumya41 <Saumyashukla4199@gmail.com>
2024-08-23 20:23:51 +0530

    minor-cloud-changes

--- app.py
+++ app.py
@@ -1,21 +1,25 @@
 import time
 import os
 import streamlit as st
-from langchain_groq import ChatGroq
+# from langchain_groq import ChatGroq
 from langchain import LLMChain, PromptTemplate
 import json
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai.chat_models import ChatOpenAI
 
 from dotenv import load_dotenv
 load_dotenv()
 
 
-# os.environ["OPENAI_API_KEY"] = "your-openai-api-key"
+openai_api_key = st.sidebar.text_input("OpenAI API Key", type="password")
+
+model = ChatOpenAI(temperature=0.7, api_key=openai_api_key)
+
 
 groq_api_key= os.getenv('GROQ_API_KEY')
 
-llm=ChatGroq(groq_api_key=groq_api_key,
-model_name="Llama3-8b-8192")
+# llm=ChatGroq(groq_api_key=groq_api_key,
+# model_name="Llama3-8b-8192")
 
 # llm = OpenAI(temperature=0.7) ##chatgpt
 
@@ -28,7 +32,7 @@ Return the questions in a JSON array format.
 """
 
 template = PromptTemplate(input_variables=["topic"], template=prompt_template)
-chain = LLMChain(llm=llm, prompt=template)
+chain = LLMChain(llm=model, prompt=template)
 
 st.title("AI Question Generator")
 st.write("Enter a topic to generate questions.")
@@ -36,15 +40,11 @@ st.write("Enter a topic to generate questions.")
 def generate_questions(topic):
     # Run the LLM chain with the provided topic
     response = chain.predict(topic=topic)
-    # # print("Raw response from the model:\n", response)
-    # try:
-    #     questions = json.loads(response)
-    #     print(questions)
-    #     return questions
-    # except json.JSONDecodeError:
-    #     return {"error": "Failed to generate questions"}
-    print(response)
-    st.write(response)
+    if not openai_api_key.startswith("sk-"):
+        st.warning("Please enter your OpenAI API key!", icon="⚠")
+    if submitted and openai_api_key.startswith("sk-"):
+        generate_response(response)
+
 
 def main():
     topic = st.text_input("Input your prompt here")
